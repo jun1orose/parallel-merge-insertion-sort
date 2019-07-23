288536b79492f1fc2b2835d8a24ad01fd9959c93
Evgene <kinddarose@gmail.com>
2019-07-23 16:47:01 +0300

    Implement AbstractTask

--- src/main/java/AbstractTask.java
+++ src/main/java/AbstractTask.java
@@ -1,3 +1,18 @@
-abstract class AbstractTask implements Runnable {
-    ThreadPool threadPool;
+abstract class AbstractTask extends Thread {
+    private ThreadPool threadPool;
+    private Runnable action;
+    final int priority;
+
+    AbstractTask(ThreadPool threadPool, int priority, Runnable action) {
+        this.threadPool = threadPool;
+        this.priority = priority;
+        this.action = action;
+    }
+
+    @Override
+    public void run() {
+        threadPool.taskStarted();
+        action.run();
+        threadPool.taskFinished();
+    }
 }
