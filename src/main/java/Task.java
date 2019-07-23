5ed1e530f19b31b4c11c2ab3b94ce99b1dec80ee
Evgene <kinddarose@gmail.com>
2019-07-23 23:28:58 +0300

    Now Task implement Thread

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,9 @@
-class AbstractTask extends Thread {
+class Task extends Thread {
     private ThreadPool threadPool;
     private Runnable action;
     final int priority;
 
-    AbstractTask(ThreadPool threadPool, int priority, Runnable action) {
+    Task(ThreadPool threadPool, int priority, Runnable action) {
         this.threadPool = threadPool;
         this.priority = priority;
         this.action = action;
