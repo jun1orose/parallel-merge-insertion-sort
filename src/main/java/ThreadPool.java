4c4098bac5f9e4460ace103c553d9db46d47a174
Evgene <kinddarose@gmail.com>
2019-07-23 10:48:22 +0300

    Change Linked to Priority for task queue

--- src/main/java/ThreadPool.java
+++ src/main/java/ThreadPool.java
@@ -1,26 +1,27 @@
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPool implements Runnable {
 
     private AtomicInteger freeThreads;
-    private LinkedBlockingQueue<Runnable> taskQueue;
+    private BlockingQueue<Runnable> taskQueue;
 
-    public ThreadPool() {
-        this.freeThreads = new AtomicInteger(6);
-        this.taskQueue = new LinkedBlockingQueue<Runnable>();
+    public ThreadPool(int nThreads) {
+        this.freeThreads = new AtomicInteger(nThreads);
+        this.taskQueue = new PriorityBlockingQueue<Runnable>();
     }
 
     void addTask(Runnable action) {
         taskQueue.add(action);
     }
 
-    void threadStarted() {
+    void taskStarted() {
         freeThreads.decrementAndGet();
     }
 
-    void threadFinished() {
+    void taskFinished() {
         freeThreads.incrementAndGet();
     }
 
