0be8217931fac8cb095fb4d46d3eb1ea32596672
Evgene <kinddarose@gmail.com>
2019-07-31 22:44:18 +0300

    Add Insertion Sort implementation

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,9 +1,23 @@
-public class InsertionSort extends AbstractTask {
 
-    InsertionSort(ThreadPool threadPool) {
-        this.threadPool = threadPool;
+public class InsertionSort implements Runnable {
+
+    private int[] array;
+
+    public InsertionSort(int[] input) {
+        this.array = input;
     }
-
+    
     public void run() {
+        for (int i = 1; i < array.length; i++) {
+            int x = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > x) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j+1] = x;
+        }
     }
 }
