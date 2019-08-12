6df92eec13827e797f717d2e2bac053d97079a55
Evgene <kinddarose@gmail.com>
2019-08-12 14:15:32 +0300

    Add new constructor for Task

--- src/main/java/threadpool/Task.java
+++ src/main/java/threadpool/Task.java
@@ -4,12 +4,16 @@ public class Task extends Thread {
     private static CustomThreadPool customThreadPool;
     private Runnable action;
     final int priority;
-    private TaskCounter countOfRelatedTasks;
+    private TaskCounter countOfRelatedTasks = new TaskCounter(0);
 
-    Task(CustomThreadPool customThreadPool, Runnable action, int priority, TaskCounter countOfRelatedTasks) {
+    public Task(CustomThreadPool customThreadPool, Runnable action, int priority) {
         Task.customThreadPool = customThreadPool;
         this.priority = priority;
         this.action = action;
+    }
+
+    public Task(CustomThreadPool customThreadPool, Runnable action, int priority, TaskCounter countOfRelatedTasks) {
+        this(customThreadPool, action, priority);
         this.countOfRelatedTasks = countOfRelatedTasks;
     }
 
