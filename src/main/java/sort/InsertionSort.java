604f0ce7c622833d12eef1f0fccc37394c900f62
Evgene <kinddarose@gmail.com>
2019-08-12 13:46:08 +0300

    Change List buffer to simple buffer

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,25 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+package sort;
 
 public class InsertionSort implements Runnable {
 
-    private List<Integer> array;
+    private int[] arr;
 
-    public InsertionSort(ArrayList<Integer> input) {
-        this.array = input;
+    public InsertionSort(int[] input) {
+        this.arr = input;
     }
     
     public void run() {
-        for (int i = 1; i <= array.size(); i++) {
-            int x = array.get(i);
+        for (int i = 1; i < arr.length; i++) {
+            int x = arr[i];
             int j = i - 1;
 
-            while (j >= 0 && array.get(j) > x) {
-                array.set(j + 1, array.get(j));
+            while (j >= 0 && arr[j] > x) {
+                arr[j + 1] = arr[j];
                 j = j - 1;
             }
 
-            array.set(j + 1, x);
+            arr[j + 1] = x;
         }
     }
 }
