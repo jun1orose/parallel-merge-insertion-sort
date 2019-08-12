a9af1fb0e0cc8c8ca51dd983ae32076ec9dd73d9
Evgene <kinddarose@gmail.com>
2019-08-12 13:40:48 +0300

    Add implementation for Merge in sorting algorithm

--- src/main/java/sort/Merge.java
+++ src/main/java/sort/Merge.java
@@ -1,14 +1,39 @@
+package sort;
+
+import java.util.List;
 
 public class Merge implements Runnable {
 
+    private int[] arr;
     private int[] leftArr;
     private int[] rightArr;
 
-    public Merge() {
-
+    public Merge(int[] input, int[] leftArr, int[] rightArr) {
+        this.arr = input;
+        this.leftArr = leftArr;
+        this.rightArr = rightArr;
     }
 
     public void run() {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < leftArr.length) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < rightArr.length) {
+            arr[k++] = rightArr[j++];
+        }
 
     }
 }
